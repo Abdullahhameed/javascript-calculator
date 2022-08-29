66b4c232be5b526f042a51b69fdcf0f326ce45a8
Abdullah Hameed <abdullahhameed66@gmail.com>
2022-08-29 23:41:56 +0500

    add log

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,6 +1,7 @@
 
 const defaultResult = 0;
 let currentResult = defaultResult;
+let logEntries = [];
 
 function getUserNumberInput() {
     return parseInt(usrInput.value);
@@ -11,19 +12,33 @@ function createAndWriteOutput(operator, resultBeforeCalc, calcNumber) {
     outputResult(currentResult, calcDescription);
 }
 
+function writeToLog(operationIdentifier, prevResult, operationNumber, newResult) {
+    const logEntry = {
+        operation: operationIdentifier,
+        prevResult: prevResult,
+        number: operationNumber,
+        result: newResult
+    }
+    logEntries.push(logEntry);
+    console.log(logEntries);
+}
+
 function add() {
     const enteredNumber = getUserNumberInput();
     const initialResult = currentResult;
-    currentResult = currentResult + enteredNumber;
+    currentResult += enteredNumber;
+    // currentResult = currentResult + enteredNumber;
     createAndWriteOutput('+', initialResult, currentResult);
-    
+    writeToLog('ADD', initialResult, enteredNumber, currentResult);
 }
+
 
 function subtract() {
     const enteredNumber = getUserNumberInput();
     const initialResult = currentResult;
     currentResult = currentResult - enteredNumber;
     createAndWriteOutput('-', initialResult, currentResult);
+    writeToLog('SUBTRACT', initialResult, enteredNumber, currentResult);
 }
 
 function multiply() {
@@ -31,6 +46,7 @@ function multiply() {
     const initialResult = currentResult;
     currentResult = currentResult * enteredNumber;
     createAndWriteOutput('*', initialResult, currentResult);
+    writeToLog('MULTIPLY', initialResult, enteredNumber, currentResult);
 }
 
 function divide() {
@@ -38,6 +54,7 @@ function divide() {
     const initialResult = currentResult;
     currentResult = currentResult / enteredNumber;
     createAndWriteOutput('/', initialResult, currentResult);
+    writeToLog('DIVISION', initialResult, enteredNumber, currentResult);
 }
 
 addBtn.addEventListener('click', add)
